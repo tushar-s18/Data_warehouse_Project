b2e13491aade54f473993041ca9284e72f5aade0
tushar-s18 <tushar109.sharma@gmail.com>
2025-10-31 01:31:54 +0530

    Add files via upload

---
+++ scripts/bronze/load_bronze_with_storedproc.sql
@@ -0,0 +1,86 @@
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	Declare @starttime datetime, @endtime datetime
+	print 'Inserting data in CRM Tables';
+
+	Set @starttime = GETDATE();
+	print 'Inserting data into:crm_cust_info';
+	Truncate table [bronze].[crm_cust_info]
+	Bulk insert [bronze].[crm_cust_info]
+	from 'C:\Users\lenovo\Desktop\Tushar\SQL Datasets\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	with(
+		firstrow= 2,
+		Fieldterminator= ',',
+		Tablock
+	);
+	Set @endtime = GETDATE();
+	print 'Load Duration ' + cast(Datediff(second, @starttime, @endtime) as varchar) + ' seconds';
+
+	Set @starttime = GETDATE();
+	print 'Inserting data into:crm_prd_info';
+	Truncate table [bronze].[crm_prd_info]
+	Bulk insert [bronze].[crm_prd_info]
+	from 'C:\Users\lenovo\Desktop\Tushar\SQL Datasets\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	with(
+		firstrow= 2,
+		Fieldterminator= ',',
+		Tablock
+	);
+	Set @endtime = GETDATE();
+	print 'Load Duration ' + cast(Datediff(second, @starttime, @endtime) as varchar) + ' seconds';
+
+	Set @starttime = GETDATE();
+	print 'Inserting data into:crm_sales_details';
+	Truncate table [bronze].[crm_sales_details]
+	Bulk insert [bronze].[crm_sales_details]
+	from 'C:\Users\lenovo\Desktop\Tushar\SQL Datasets\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	with(
+		firstrow= 2,
+		Fieldterminator= ',',
+		Tablock
+	);
+	Set @endtime = GETDATE();
+	print 'Load Duration ' + cast(Datediff(second, @starttime, @endtime) as varchar) + ' seconds';
+
+print'-------------------------------';
+	print 'Inserting data in ERP tables';
+
+	Set @starttime = GETDATE();
+	print 'Inserting data into:erp_cust_az12';
+	Truncate table [bronze].[erp_cust_az12]
+	Bulk insert [bronze].[erp_cust_az12]
+	from 'C:\Users\lenovo\Desktop\Tushar\SQL Datasets\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	with(
+		firstrow= 2,
+		Fieldterminator= ',',
+		Tablock
+	);
+	Set @endtime = GETDATE();
+	print 'Load Duration ' + cast(Datediff(second, @starttime, @endtime) as varchar) + ' seconds';
+
+	Set @starttime = GETDATE();
+	print 'Inserting data into:erp_loc_a101';
+	Truncate table [bronze].[erp_loc_a101]
+	Bulk insert [bronze].[erp_loc_a101]
+	from 'C:\Users\lenovo\Desktop\Tushar\SQL Datasets\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+	with(
+		firstrow= 2,
+		Fieldterminator= ',',
+		Tablock
+	);
+    Set @endtime = GETDATE();
+	print 'Load Duration ' + cast(Datediff(second, @starttime, @endtime) as varchar) + ' seconds';
+
+	Set @starttime = GETDATE();
+	print 'Inserting data into:erp_px_cat_g1v2';
+	Truncate table [bronze].[erp_px_cat_g1v2]
+	Bulk insert [bronze].[erp_px_cat_g1v2]
+	from 'C:\Users\lenovo\Desktop\Tushar\SQL Datasets\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	with(
+		firstrow= 2,
+		Fieldterminator= ',',
+		Tablock
+	);
+	Set @endtime = GETDATE();
+	print 'Load Duration ' + cast(Datediff(second, @starttime, @endtime) as varchar) + ' seconds';
+END
